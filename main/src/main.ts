0443a2044bd4b1839e0118b6bb5d6feb2bc63f75
rado1999 <dova.jarvis@gmail.com>
2022-08-01 19:44:24 +0500

    some change

--- main/src/main.ts
+++ main/src/main.ts
@@ -5,7 +5,9 @@ import { join } from 'path'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule)
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+        logger: false
+    })
     app.useStaticAssets(join(__dirname, '..', 'public'))
     app.enableCors()
     app.useGlobalPipes(new ValidationPipe({ transform: true }))
