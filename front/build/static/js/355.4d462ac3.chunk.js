589e3eb7b9f4901b41f4b63478f6623ad5bbf8ae
rado1999 <dova.jarvis@gmail.com>
2022-08-02 15:11:56 +0500

    some changes

--- front/build/static/js/355.4d462ac3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksumbar=self.webpackChunksumbar||[]).push([[355],{6355:function(t,c,u){u.d(c,{iUH:function(){return n}});var a=u(9983);function n(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 320 512"},child:[{tag:"path",attr:{d:"M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"}}]})(t)}}}]);
-//# sourceMappingURL=355.4d462ac3.chunk.js.map
